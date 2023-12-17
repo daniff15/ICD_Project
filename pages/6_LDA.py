3cffb902e98d67c21b081750beda3e7a02d522fb
filipefreixo1 <filipefreixo1@ua.pt>
2023-12-17 00:47:25 +0000

    create function to choose the country that will be displayed

--- pages/6_LDA.py
+++ pages/6_LDA.py
@@ -11,67 +11,16 @@ from nltk.stem import PorterStemmer
 import re
 from gensim.models.coherencemodel import CoherenceModel
 import gensim.corpora as corpora
+from nbimporter import NotebookLoader
 
+# Especifique o caminho do arquivo Jupyter Notebook (substitua 'notebook.ipynb' pelo caminho do seu arquivo)
+loader = NotebookLoader()
+main = loader.load_module("./main.ipynb")
 
-# Baixar os recursos necessários para o NLTK
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-
-# Suprimir alguns avisos devido a versões antigas do pyLDAvis
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-# Load the dataset
-file_path = "./data/icd_scopus.csv"
-df = pd.read_csv(file_path)
-
-# Processamento de texto para LDA
-stop_words = set(stopwords.words('english'))
-ps = PorterStemmer()
-
-def preprocess_text(text):
-    # Remover caracteres especiais, números e converter para minúsculas
-    text = re.sub(r'[^a-zA-Z]', ' ', str(text))
-    text = text.lower()
-    
-    # Tokenização
-    words = word_tokenize(text)
-    
-    # Remover stopwords e realizar stemming
-    words = [ps.stem(word) for word in words if word not in stop_words and len(word) > 2]
-    
-    return words
-
-# Aplicar pré-processamento aos resumos
-df['Processed_Abstract'] = df['Abstract'].apply(preprocess_text)
-
-# Remover documentos sem resumo após o pré-processamento
-df = df[df['Processed_Abstract'].apply(len) > 0]
-
-# Criar um dicionário e um corpus Gensim
-dictionary = Dictionary(df['Processed_Abstract'])
-corpus_gensim = [dictionary.doc2bow(text) for text in df['Processed_Abstract']]
-
-# Treinar o modelo LDA
-num_topics = 5  # Especifique o número desejado de tópicos
-lda_model = LdaModel(corpus_gensim, num_topics=num_topics, id2word=dictionary)
-
-# Criar a visualização do LDA
-vis_data = gensimvis.prepare(lda_model, corpus_gensim, dictionary)
+vis = main.lda_unique_country("Portugal")
 
 # Salvar a visualização como um arquivo HTML
-pyLDAvis.save_html(vis_data, 'lda_visualization.html')
+pyLDAvis.save_html(vis, 'lda_visualization.html')
 
 # Exibir o gráfico no Streamlit usando st.components
 st.components.v1.html(open('lda_visualization.html', 'r', encoding='utf-8').read(), height=800, width=1200)
-
-
-processed_texts = _preprocess_data(df)
-
-# Create Dictionary
-id2word = corpora.Dictionary(processed_texts)
-
-coherence_model_lda = CoherenceModel(model=lda_model, texts=processed_texts, dictionary=id2word, coherence='c_v')
-coherence_lda = coherence_model_lda.get_coherence()
-st.write('Coherence Score:', coherence_lda)
